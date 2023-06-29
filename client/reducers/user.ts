9cc10c080d505a134d66573c336b06a7553c04b6
Lauren Lancaster <737lanc@gmail.com>
2023-06-29 15:28:12 +1200

    fix actions

--- client/reducers/user.ts
+++ client/reducers/user.ts
@@ -1,6 +1,6 @@
 import { UserAction, FINISH_GAME } from '../actions/user'
 
-import { User } from '../../models/user'
+import { User } from '../../models/users'
 
 const initialState = [] as User[]
 
--- server/db/users.ts
+++ server/db/users.ts
@@ -1,8 +1,10 @@
-import { User } from "../../models/users";
-import connection from "./connection";
+import { User } from '../../models/users'
+import connection from './connection'
 
 const db = connection
 
-export function addUser(user: User){
+export function addUser(user: User) {
   return db('users').insert(user)
 }
+
+export function completeGame(user: User)
