33ab435df6378fc806c1ebe5ac4f1b3740040132
Gemma Krebs <gemmakrebs@live.com>
2023-06-29 14:05:02 +1200

    add Start page and begin design

--- client/components/Start.tsx
+++ client/components/Start.tsx
@@ -1,1 +1,21 @@
-g
+function Start() {
+  return (
+    <>
+      <h1>this is the start page</h1>
+      <div id="esc-logo-container">
+        <div id="esc-logo">
+          {/* 3JS Logo goes here */}
+          <p>3JS ESC LOGO</p>
+        </div>
+      </div>
+      <form>
+        <label htmlFor=""></label>
+        <input type="text" />
+        <label htmlFor=""></label>
+        <input type="text" />
+      </form>
+    </>
+  )
+}
+
+export default Start
