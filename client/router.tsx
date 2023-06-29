4a6fe8ffddf95a6932ed8db987ce18dd18932eb8
Gabriela Perez <gaby@perez.co.nz>
2023-06-29 16:03:55 +1200

    changed main to start, deleted main

--- client/router.tsx
+++ client/router.tsx
@@ -5,14 +5,14 @@ import {
 } from 'react-router-dom'
 
 import App from './components/App'
-import Main from './components/Main'
+import Start from './components/Start'
 import Example from './components/Example'
 import Elevator from './components/Elevator'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index element={<Main />} />
+      <Route index element={<Start />} />
       <Route path="/example" element={<Example />}></Route>
       <Route path="/game/evelator" element={<Elevator />}></Route>
       {/* <Route path="path/:params" element={<Component />}></Route> */}
