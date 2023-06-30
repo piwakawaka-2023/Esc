5e5f18ea6982f24faf74b0026a5a9de2b73900e7
Jesse <j.t.a.smith26@gmail.com>
2023-06-30 14:18:10 +1200

    Added nav bar to game component

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -6,6 +6,7 @@ import { useSound } from 'use-sound'
 import ambienceUrl from '/sounds/ambience.wav'
 import { useState } from 'react'
 import slackUrl from '/sounds/wow.mp3'
+import { GameNav } from '../components/GameNav'
 
 export default function Game() {
   // const user = useAppSelector((state) => state.user) as User
@@ -28,6 +29,7 @@ export default function Game() {
 
   return (
     <>
+      <GameNav />
       <h1>...do you want to play a game?</h1>
       <Outlet />
     </>
---
+++ client/components/GameNav.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+function GameNav() {
+  return (
+    <ul>
+      <Link to="/">
+        <li>Escape</li>
+      </Link>
+    </ul>
+  )
+}
+
+export default GameNav
