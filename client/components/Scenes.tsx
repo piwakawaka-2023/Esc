4f61654839e99e8545510009b618c71133a6074c
Jesse <j.t.a.smith26@gmail.com>
2023-06-30 15:33:53 +1200

    fixed other bug

--- client/components/Scenes.tsx
+++ client/components/Scenes.tsx
@@ -31,17 +31,7 @@ export function getScenes() {
           </div>
           <span></span>
           <Link to={`level/${curScene?.levelId}`}>Next</Link>
-          <div className="scene-card" onClick={() => handlePlay()}>
-            <div className="scene-card">
-              <div className="scene-card-content">
-                <div className="slack-card-logo"></div>
-                <div className="slack-message">
-                  <p>{curScene?.text}</p>
-                </div>
-                <Link to={`level/${curScene?.levelId}`}>Next</Link>
-              </div>
-            </div>
-          </div>
+          <div className="scene-card" onClick={() => handlePlay()}></div>
         </div>
       </div>
     </>
