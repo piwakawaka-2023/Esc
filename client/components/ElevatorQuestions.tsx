bf8c96aca0950dcb8860d2377413bb19edef2a19
Gemma Krebs <gemmakrebs@live.com>
2023-07-03 10:56:24 +1200

    code tidy

--- client/components/ElevatorQuestions.tsx
+++ client/components/ElevatorQuestions.tsx
@@ -1,25 +1,10 @@
-import { SetStateAction, useEffect, useState, Dispatch } from 'react'
+import { SetStateAction, useEffect, Dispatch } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/hooks'
 import * as actions from '../actions/questions'
 import { Question } from '../../models/questions'
 import useSound from 'use-sound'
 import incorrectBuzzerUrl from '/sounds/wrong-buzzer.mp3'
 import correctBuzzerUrl from '/sounds/correct-buzzer.mp3'
-
-//currently receives levelnumber as props so the correct question can be loaded
-//needs to be able to take two parameters, one for the levelnumber and a second for a function
-//the function inside the parent component (elevator) will update the state to store when a question has been passed
-//then the state can be used to move through levels.
-
-//the Component will look similar to this
-{
-  /* <ElevatorQuestions data={levelNum} changeMessage={changeMessage}/> */
-}
-
-//the parent component will take a function and a state, similar to below
-
-//the function will be called inside the onclick "checkAnswer" functions set below
-//state to show correct or fail to be sent back to parent
 
 interface Props {
   questionPassed: boolean
@@ -64,25 +49,25 @@ function ElevatorQuestions(props: Props) {
           onClick={() => checkAnswer(question?.answer1)}
           className="blue-button blue-button-lge"
         >
-          {question?.answer1}
+          1.{question?.answer1}
         </button>
         <button
           onClick={() => checkAnswer(question?.answer2)}
           className="blue-button blue-button-lge"
         >
-          {question?.answer2}
+          2.{question?.answer2}
         </button>
         <button
           onClick={() => checkAnswer(question?.answer3)}
           className="blue-button blue-button-lge"
         >
-          {question?.answer3}
+          3.{question?.answer3}
         </button>
         <button
           onClick={() => checkAnswer(question?.answer4)}
           className="blue-button blue-button-lge"
         >
-          {question?.answer4}
+          4.{question?.answer4}
         </button>
       </div>
     </div>
