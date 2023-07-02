f684c7de59243c1231639df04928c0677e9811a0
Lauren Lancaster <737lanc@gmail.com>
2023-07-02 15:38:41 +1200

    timer works

--- client/components/Timer.tsx
+++ client/components/Timer.tsx
@@ -4,41 +4,60 @@ import timerReducer from '../reducers/timer'
 
 export default function Timer() {
   const [time, setTime] = useState(0)
+  const [intervalId, setIntervalId] = useState(0)
   //const [isRunning, setIsRunning] = useState(true)
 
   const toggleTimer = useAppSelector((state) => state.timer) as boolean
   //const [isRunning, setRunning] = useState(false)
 
-  // useEffect(() => {
-  //   setRunning(toggleTimer)
-  // }, [toggleTimer])
+  useEffect(() => {
+    if (toggleTimer) {
+      console.log('true')
+      runTimer()
+    } else if (!toggleTimer) {
+      console.log('false')
+      stopTimer()
+    }
+  }, [toggleTimer])
 
   ///in welcome and complete I call toggleTimer with a true/false parameter
   //that much is working
 
-  useEffect(() => {
-    let intervalId: any
-    if (toggleTimer) {
-      intervalId = setInterval(() => setTime(time + 1), 1000)
-      console.log('why')
-    } else if (!toggleTimer) {
-      console.log('stop')
-      //else if (toggleTimer === false) {
-      //console.log('stop')
-      //setTime(0)
-      clearInterval(intervalId)
-      setTime(0)
-    }
-  }, [toggleTimer, time])
+  // useEffect(() => {
+  //   let intervalId: any
+  //   if (toggleTimer) {
+  //     intervalId = setInterval(() => setTime(time + 1), 1000)
+  //     console.log('why')
+  //   } else if (!toggleTimer) {
+  //     console.log('stop')
+  //     //else if (toggleTimer === false) {
+  //     //console.log('stop')
+  //     //setTime(0)
+  //     clearInterval(intervalId)
+  //     setTime(0)
+  //   }
+  // }, [toggleTimer, time])
 
-  const seconds = Math.floor((time % 6000) / 100)
-  const milliseconds = time % 100
+  function runTimer() {
+    const newIntervalId = setInterval(() => {
+      setTime((time) => time + 1)
+    }, 1000)
+    setIntervalId(newIntervalId)
+  }
+
+  function stopTimer() {
+    clearInterval(intervalId)
+  }
+
+  // const seconds = Math.floor((time % 6000) / 100)
+  // const milliseconds = time % 100
 
   return (
     <p className="stopwatch-time">
       Timer:
-      {seconds.toString().padStart(2, '0')}:
-      {milliseconds.toString().padStart(2, '0')}
+      {/* {seconds.toString().padStart(2, '0')}:
+      {milliseconds.toString().padStart(2, '0')} */}
+      {time}
     </p>
   )
 }
