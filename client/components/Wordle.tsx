045c07eaf2456b9ca2d166bbbf96afeec32f9048
Jesse <j.t.a.smith26@gmail.com>
2023-07-03 11:44:23 +1200

    wordle routered and playable

--- client/components/Wordle.tsx
+++ client/components/Wordle.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+// import { useParams, Link } from 'react-router-dom'
 
-export default function Wordle() {
-  const { userId } = useParams()
+export function Wordle() {
+  // const { userId } = useParams()
 
   //Run JS on this comp
   useEffect(() => {
     const script = document.createElement('script')
     script.src = '/scripts/wordle.js'
-    script.async = true
+    console.log(script.src, 'script.src')
+    script.async = false
     document.body.appendChild(script)
+    console.log(script, 'script appendchild')
 
     //Attaching CSS to this comp
     const link = document.createElement('link')
-    link.href = 'styles/wordle.css'
+    link.href = '/styles/wordle.css'
     link.rel = 'stylesheet'
     link.type = 'text/css'
     document.head.appendChild(link)
@@ -28,16 +30,67 @@ export default function Wordle() {
     //clean component when finished being used.
     return () => {
       document.body.removeChild(script)
-      document.head.removeChild(link)
       document.head.removeChild(link2)
+      document.head.removeChild(link)
     }
   }, [])
 
   return (
     <>
-      <div>
-        <Link to={`/game/${userId}/scene/2`}>Exit</Link>
+      <div id="container">
+        <div id="game">
+          <header>
+            <h1 className="title">ENTER PASSCODE</h1>
+          </header>
+          <div className="board-container">
+            <div id="board"></div>
+
+            <div id="keyboard-container">
+              <div className="keyboard-row">
+                <button data-key="q">q</button>
+                <button data-key="w">w</button>
+                <button data-key="e">e</button>
+                <button data-key="r">r</button>
+                <button data-key="t">t</button>
+                <button data-key="y">y</button>
+                <button data-key="u">u</button>
+                <button data-key="i">i</button>
+                <button data-key="o">o</button>
+                <button data-key="p">p</button>
+              </div>
+              <div className="keyboard-row">
+                <div className="spacer-half"></div>
+                <button data-key="a">a</button>
+                <button data-key="s">s</button>
+                <button data-key="d">d</button>
+                <button data-key="f">f</button>
+                <button data-key="g">g</button>
+                <button data-key="h">h</button>
+                <button data-key="j">j</button>
+                <button data-key="k">k</button>
+                <button data-key="l">l</button>
+              </div>
+              <div className="keyboard-row">
+                <button data-key="enter" className="wide-button">
+                  Enter
+                </button>
+                <button data-key="z">z</button>
+                <button data-key="x">x</button>
+                <button data-key="c">c</button>
+                <button data-key="v">v</button>
+                <button data-key="b">b</button>
+                <button data-key="n">n</button>
+                <button data-key="m">m</button>
+                <button data-key="del" className="wide-button">
+                  Del
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
 }
+
+export default Wordle
--- public/scripts/wordle.js
+++ public/scripts/wordle.js
@@ -1,117 +1,116 @@
-document.addEventListener('DOMContentLoaded', () => {
-  createSquares()
+createSquares()
 
-  let guessedWords = [[]]
-  let availableSpace = 1
+let guessedWords = [[]]
+let availableSpace = 1
 
-  let word = 'proxy'
-  let guessWordCount = 0
+let word = 'proxy'
+let guessWordCount = 0
 
-  const keys = document.querySelectorAll('.keyboard-row button')
+const keys = document.querySelectorAll('.keyboard-row button')
 
-  function getCurrentWordArr() {
-    const numberOfGuessedWords = guessedWords.length
-    return guessedWords[numberOfGuessedWords - 1]
+function getCurrentWordArr() {
+  const numberOfGuessedWords = guessedWords.length
+  return guessedWords[numberOfGuessedWords - 1]
+}
+
+function updateGuessedWords(letter) {
+  const currentWordArr = getCurrentWordArr()
+  if (currentWordArr && currentWordArr.length < 5) {
+    currentWordArr.push(letter)
+
+    const availableSpaceEl = document.getElementById(String(availableSpace))
+    availableSpace = availableSpace + 1
+
+    availableSpaceEl.textContent = letter
+  }
+}
+
+function getTileColor(letter, index) {
+  const isCorrectLetter = word.includes(letter)
+
+  if (!isCorrectLetter) {
+    return 'rgb(58,58,60)'
   }
 
-  function updateGuessedWords(letter) {
-    const currentWordArr = getCurrentWordArr()
-    if (currentWordArr && currentWordArr.length < 5) {
-      currentWordArr.push(letter)
+  const letterInThatPosition = word.charAt(index)
+  const isCorrectPosition = letter === letterInThatPosition
 
-      const availableSpaceEl = document.getElementById(String(availableSpace))
-      availableSpace = availableSpace + 1
-
-      availableSpaceEl.textContent = letter
-    }
+  if (isCorrectPosition) {
+    return 'rgb(83, 141,78)'
   }
 
-  function getTileColor(letter, index) {
-    const isCorrectLetter = word.includes(letter)
+  return 'rgb(181,159,59)'
+}
 
-    if (!isCorrectLetter) {
-      return 'rgb(58,58,60)'
-    }
-
-    const letterInThatPosition = word.charAt(index)
-    const isCorrectPosition = letter === letterInThatPosition
-
-    if (isCorrectPosition) {
-      return 'rgb(83, 141,78)'
-    }
-
-    return 'rgb(181,159,59)'
+function handleSubmitWord() {
+  const currentWordArr = getCurrentWordArr()
+  if (currentWordArr.length !== 5) {
+    window.alert('Password must be 5 letters')
   }
 
-  function handleSubmitWord() {
-    const currentWordArr = getCurrentWordArr()
-    if (currentWordArr.length !== 5) {
-      window.alert('Password must be 5 letters')
-    }
+  const currentWord = currentWordArr.join('')
 
-    const currentWord = currentWordArr.join('')
+  const firstLetterId = guessWordCount * 5 + 1
+  const interval = 200
+  currentWordArr.forEach((letter, index) => {
+    setTimeout(() => {
+      const tileColor = getTileColor(letter, index)
 
-    const firstLetterId = guessWordCount * 5 + 1
-    const interval = 200
-    currentWordArr.forEach((letter, index) => {
-      setTimeout(() => {
-        const tileColor = getTileColor(letter, index)
+      const letterId = firstLetterId + index
+      const letterEl = document.getElementById(letterId)
+      letterEl.classList.add('animate__flipInX')
+      letterEl.style = `background-color:${tileColor};border-color:${tileColor}`
+    }, interval * index)
+  })
 
-        const letterId = firstLetterId + index
-        const letterEl = document.getElementById(letterId)
-        letterEl.classList.add('animate__flipInX')
-        letterEl.style = `background-color:${tileColor};border-color:${tileColor}`
-      }, interval * index)
-    })
+  guessWordCount += 1
 
-    guessWordCount += 1
-
-    if (currentWord === word) {
-      window.alert('Congratulations')
-    }
-    if (guessedWords.length === 6) {
-      window.alert(`Sorry you have no more guesses, the password is ${word}`)
-    }
-    guessedWords.push([])
+  if (currentWord === word) {
+    window.alert('Congratulations')
   }
+  if (guessedWords.length === 6) {
+    window.alert(`Sorry you have no more guesses, the password is ${word}`)
+  }
+  guessedWords.push([])
+}
 
-  function createSquares() {
-    const gameBoard = document.getElementById('board')
-    for (let index = 0; index < 30; index++) {
-      let square = document.createElement('div')
-      square.classList.add('square')
-      square.classList.add('animate__animated')
-      square.setAttribute('id', index + 1)
-      gameBoard.appendChild(square)
+function createSquares() {
+  const gameBoard = document.getElementById('board')
+  for (let index = 0; index < 30; index++) {
+    let square = document.createElement('div')
+    square.classList.add('square')
+    square.classList.add('animate__animated')
+    square.setAttribute('id', index + 1)
+    gameBoard.appendChild(square)
+  }
+}
+
+function handleDeleteLetter() {
+  const currentWordArr = getCurrentWordArr()
+  const removedLetter = currentWordArr.pop()
+
+  guessedWords[guessedWords.length - 1] = currentWordArr
+
+  const lastLetterEl = document.getElementById(String(availableSpace - 1))
+
+  lastLetterEl.textContent = ''
+  availableSpace = availableSpace - 1
+}
+
+for (let i = 0; i < keys.length; i++) {
+  keys[i].onclick = ({ target }) => {
+    const letter = target.getAttribute('data-key')
+
+    if (letter === 'enter') {
+      handleSubmitWord()
+      return
     }
-  }
 
-  function handleDeleteLetter() {
-    const currentWordArr = getCurrentWordArr()
-    const removedLetter = currentWordArr.pop()
-
-    guessedWords[guessedWords.length - 1] = currentWordArr
-
-    const lastLetterEl = document.getElementById(String(availableSpace - 1))
-
-    lastLetterEl.textContent = ''
-    availableSpace = availableSpace - 1
-  }
-
-  for (let i = 0; i < keys.length; i++) {
-    keys[i].onclick = ({ target }) => {
-      const letter = target.getAttribute('data-key')
-
-      if (letter === 'enter') {
-        handleSubmitWord()
-        return
-      }
-
-      if (letter === 'del') {
-        handleDeleteLetter()
-        return
-      }
-      updateGuessedWords(letter)
+    if (letter === 'del') {
+      handleDeleteLetter()
+      return
     }
+    updateGuessedWords(letter)
   }
-})
+}
+// })
