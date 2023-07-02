aa8615b8d26502c9120a6749b9c61afb9d7138b6
Gemma Krebs <gemmakrebs@live.com>
2023-07-02 14:49:28 +1200

    redux thunk being weird

--- client/components/ElevatorQuestions.tsx
+++ client/components/ElevatorQuestions.tsx
@@ -11,7 +11,14 @@ function ElevatorQuestions() {
   }, [dispatch])
 
   const questions = useAppSelector((state) => state.question) as Question[]
-  console.log(questions)
+
+  const checkAnswer = (answer: string | undefined) => {
+    if (answer == questions[0].correct) {
+      console.log('you were right')
+    } else {
+      console.log("you're wrong")
+    }
+  }
 
   return (
     <div className="screen screen-sml">
@@ -23,16 +30,28 @@ function ElevatorQuestions() {
           justifyContent: 'space-evenly',
         }}
       >
-        <button className="blue-button blue-button-lge">
+        <button
+          onClick={() => checkAnswer(questions[0].answer1)}
+          className="blue-button blue-button-lge"
+        >
           {questions[0].answer1}
         </button>
-        <button className="blue-button blue-button-lge">
+        <button
+          onClick={() => checkAnswer(questions[0].answer2)}
+          className="blue-button blue-button-lge"
+        >
           {questions[0].answer2}
         </button>
-        <button className="blue-button blue-button-lge">
+        <button
+          onClick={() => checkAnswer(questions[0].answer3)}
+          className="blue-button blue-button-lge"
+        >
           {questions[0].answer3}
         </button>
-        <button className="blue-button blue-button-lge">
+        <button
+          onClick={() => checkAnswer(questions[0].answer4)}
+          className="blue-button blue-button-lge"
+        >
           {questions[0].answer4}
         </button>
       </div>
