6f72c6923524032c16cf94a39c21f99ab4a4a061
Jesse <j.t.a.smith26@gmail.com>
2023-07-04 09:50:48 +1200

    hints on lauren

--- client/components/LightsOff.tsx
+++ client/components/LightsOff.tsx
@@ -21,6 +21,17 @@ export default function LightsOff() {
           </button>
         </Link>
       )}
+      <div>
+        <p>Hint</p>
+        <p>
+          Starting with the second row, click on every cell that has a light on
+          in the row above it. This will turn off all the lights in that row.
+          Continue with each successive row until the only remaining lights are
+          in the final row. In the top row, select the inverse of the bottom row
+          of cells with lights on. FOR EXAMPLE Bottom row: -*- Top row: *-*,
+          Bottom row: *-- Top row: --* Repeat
+        </p>
+      </div>
     </>
   )
 }
