7882fd18e455563af54479c05e9e209052333033
Gemma Krebs <gemmakrebs@live.com>
2023-07-03 14:22:30 +1200

    complete basic elevator code for bottom of elevator

--- client/components/ElevatorCode.tsx
+++ client/components/ElevatorCode.tsx
@@ -1,19 +1,55 @@
-function ElevatorCode() {
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { SetStateAction, Dispatch } from 'react'
+
+import useSound from 'use-sound'
+import incorrectBuzzerUrl from '/sounds/wrong-buzzer.mp3'
+import correctBuzzerUrl from '/sounds/correct-buzzer.mp3'
+
+interface Props {
+  codeCracked: boolean
+  setCodeCracked: Dispatch<SetStateAction<boolean>>
+}
+
+function ElevatorCode(props: Props) {
+  const [codeAnswer, setCodeAnswer] = useState('')
+  const [playIncorrectBuzzer] = useSound(incorrectBuzzerUrl, { volume: 0.05 })
+  const [playCorrectBuzzer] = useSound(correctBuzzerUrl, { volume: 0.2 })
+
+  const pincode = '2421'
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setCodeAnswer(evt.target.value)
+  }
+
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault()
+    if (codeAnswer === pincode) {
+      playCorrectBuzzer()
+      props.setCodeCracked(true)
+    } else {
+      playIncorrectBuzzer()
+      props.setCodeCracked(false)
+    }
+  }
+
   return (
     <>
-    <div className="screen screen-sml">
-      <h2>Enter the pincode</h2>
-    </div>
-      <p>this is the elevator code</p>
+      <div className="screen screen-sml">
+        <h2>Enter the pincode</h2>
+        <form onSubmit={handleSubmit}>
+          {/* <label htmlFor="pincode"></label> */}
+          <input
+            className="input-field"
+            type="number"
+            min={1000}
+            max={9999}
+            onChange={handleChange}
+          />
+          <button className="blue-button">Guess</button>
+        </form>
+      </div>
     </>
   )
 }
 
 export default ElevatorCode
-
-//asks user to enter pin code ()
-//hard code the pincode into a variable
-//check the users pincode against variable
-//if correct show the open doors elevator button
-//if incorrect show user is wrong
-//offer a hint? 
