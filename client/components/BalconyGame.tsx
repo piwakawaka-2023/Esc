4a6faa855681dc23e11cdc788f5e7f3d9b6419cd
Gabriela Perez <gaby@perez.co.nz>
2023-07-03 14:25:53 +1200

    next button on balcony game

--- client/components/BalconyGame.tsx
+++ client/components/BalconyGame.tsx
@@ -1,22 +1,33 @@
+import { useState } from 'react'
 import BalonyStatsScreen from './BalconyStatsScreen'
-import purpleVape from '/images/vape-purple.png'
 import bus from '/images/bus.png'
+import { Link, useParams } from 'react-router-dom'
 
 export default function BalconyGame() {
+  const [nextButton, setnextButton] = useState(true)
+  const { userId } = useParams()
+
   const vapeCatch = 0
   const pipeCatch = 0
-  const missed = 0
+
   return (
     <>
-      <BalonyStatsScreen
-        vapeCatch={vapeCatch}
-        pipeCatch={pipeCatch}
-        missed={missed}
-      />
+      <BalonyStatsScreen vapeCatch={vapeCatch} pipeCatch={pipeCatch} />
       <div className="balony-game-board">
         <div className="vape"> </div>
+        <div className="pipes"> </div>
         <img src={bus} alt="yellow bus" id="bus" />
       </div>
+      {nextButton && (
+        <Link to={`/game/${userId}/scene/4`}>
+          <button
+            style={{ position: 'fixed', bottom: '0' }}
+            className="blue-button"
+          >
+            Exit
+          </button>
+        </Link>
+      )}
     </>
   )
 }
