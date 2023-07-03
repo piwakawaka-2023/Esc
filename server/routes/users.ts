41ee5ead43f35cd5e0c7c986700c5ce82904fd97
Gemma Krebs <gemmakrebs@live.com>
2023-07-04 09:41:47 +1200

    clean back end code

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import * as db from '../db/users'
-import { idText } from 'typescript'
 
 const router = express.Router()
 
@@ -60,7 +59,7 @@ router.patch('/:id', async (req, res) => {
 })
 
 router.patch('/updatetime/:id', async (req, res, next) => {
-  const id = +req.params.id //could be user.id ?
+  const id = +req.params.id
   const time = req.body.time
   try {
     await db.updatePlayingTime(id, time)
