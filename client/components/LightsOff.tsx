6e66316345f140e89e1cd6d3647eb2302ccf8b27
Lauren Lancaster <737lanc@gmail.com>
2023-07-04 14:38:24 +1200

    gameover route in lights off

--- client/components/LightsOff.tsx
+++ client/components/LightsOff.tsx
@@ -1,11 +1,7 @@
 import Board from './LightsOffBoard'
 import { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
 
 export default function LightsOff() {
-  const { userId } = useParams()
-
-  const [nextButton, setnextButton] = useState(true)
   const [hintsButton, setHintsButton] = useState(false)
 
   const handleClick = () => {
@@ -20,18 +16,6 @@ export default function LightsOff() {
           <Board />
         </div>
       </div>
-      {/* {nextButton && (
-        <Link to={`/game/${userId}/scene/1/level/2`}>
-          <button
-            style={{ position: 'fixed', bottom: '0' }}
-            className="blue-button"
-          >
-            Escape
-          </button>
-
-        )} }
-        </Link>
-      )} */}
       <div>
         <p onClick={handleClick}>Hint</p>
         {hintsButton && (
