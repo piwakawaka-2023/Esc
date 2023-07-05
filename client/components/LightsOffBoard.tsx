d9bb60584ad52fc1359b4366e6ad5bb15ca3af5e
Gabriela Perez <gaby@perez.co.nz>
2023-07-05 13:34:18 +1200

    red screen blicking lights off

--- client/components/LightsOffBoard.tsx
+++ client/components/LightsOffBoard.tsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 
 function Board() {
+  const [colourScreen, setColourScreen] = useState('yellow-screen')
+  const [colourText, setColourText] = useState('')
+  const [colourBox, setBoxColour] = useState('')
+
   const size = 3
   const chanceLightStartsOn = 0.25
   const [count, setCount] = useState(0)
@@ -61,6 +65,11 @@ function Board() {
 
   const incCounter = () => {
     setCount(count + 1)
+    if (count > 18) {
+      setColourScreen('red-screen')
+      setColourText('red-text')
+      setBoxColour('red-box')
+    }
   }
 
   const gridDisplay = board.grid.map(function (row, rowIndex) {
@@ -84,10 +93,14 @@ function Board() {
 
   return (
     <>
-      <div className="screen" id="yellow-screen">
-        <p className="lightoff-header">Turn Off the Lights</p>
-        <div className="lightoff-attempts">
-          <p className='number'>Attempts left: {25 - count} </p>
+      <div className="screen red-blinking" id={colourScreen}>
+        <p className="lightoff-header" id={colourText}>
+          Turn Off the Lights
+        </p>
+        <div className="lightoff-attempts" id={colourBox}>
+          <p className="number" id={colourText}>
+            Attempts left: {25 - count}{' '}
+          </p>
         </div>
       </div>
       <div className="board">
