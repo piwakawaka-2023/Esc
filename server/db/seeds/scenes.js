8cfcc9c391acfcf79e7a23b48441d1e0b09fe427
Jesse <j.t.a.smith26@gmail.com>
2023-07-05 14:26:26 +1200

    seed fix

--- server/db/seeds/scenes.js
+++ server/db/seeds/scenes.js
@@ -9,7 +9,7 @@ exports.seed = async function (knex) {
     {
       id: 1,
       level_id: 1,
-      text: "Hey, Cheers for sticking back to finish up the app deployment! Please dont forget to turn off the lights and close down before you lock up for the night. Also, the elevator repair guys will be in tomorrow instead- so just a reminder to be careful in the elevator! Not that you'll have any trouble with that haha. But give me a shout if you get stuck. See you tomorrow for the teams meeting!",
+      text: "Hey, Cheers for sticking back to finish up the app deployment! Please dont forget to turn off the lights and close down before you lock up for the night. Also, the elevator repair guys will be in tomorrow - so just a reminder to be careful in the elevator! Not that you'll have any trouble with that haha. But give me a shout if you get stuck. See you tomorrow for the teams meeting!",
       final: false,
       slack:
         "Not everyone is a 'Guy'... 'Guys' is a gendered pronoun. We recommend an alternative like 'crew'. We appreciate your help in building an inclusive environment at Dev!",
@@ -20,7 +20,7 @@ exports.seed = async function (knex) {
       text: "Me again... Just checking if you're still in the building? I left my swipe card in the basement if you're still around maybe you could grab it for me? Sorry I should've asked before you turned off all the lights. Don't mind the clutter, I'm going to clean it tomorrow, let me know how you go!",
       final: false,
       slack:
-        'You asked me to remind you about this message from @facilitator-piwakawaka-23',
+        '👍 I will remind you about the #teams-meeting at 10:30AM tomorrow morning.',
     },
     {
       id: 3,
@@ -35,7 +35,7 @@ exports.seed = async function (knex) {
       text: "Oh, you made it out of the building? Well, thats great then! Uhh... I suppose we'll see you tomorrow then.",
       final: false,
       slack:
-        ' 👍 I will remind you about the #teams-meeting at 10:30AM tomorrow morning.',
+        'You asked me to remind you about this message from @facilitator-piwakawaka-23',
     },
     {
       id: 5,
