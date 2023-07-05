44e5777a5556e13cc898754ebe1ed3918f7454e3
Jesse <j.t.a.smith26@gmail.com>
2023-07-05 13:27:48 +1200

    grammar check

--- server/db/seeds/scenes.js
+++ server/db/seeds/scenes.js
@@ -9,15 +9,15 @@ exports.seed = async function (knex) {
     {
       id: 1,
       level_id: 1,
-      text: "Hey, Cheers for sticking back to finish up the app deploy! Please dont forget to turn off the lights and close down before you lock up for the night. Also, the elevator repair guys will be in tomorrow instead- so just a reminder to be careful in the elevator! not that you'll have any trouble with that haha. But give me a shout if you get stuck. See you tomorrow for the teams meeting!",
+      text: "Hey, Cheers for sticking back to finish up the app deployment! Please dont forget to turn off the lights and close down before you lock up for the night. Also, the elevator repair guys will be in tomorrow instead- so just a reminder to be careful in the elevator! not that you'll have any trouble with that haha. But give me a shout if you get stuck. See you tomorrow for the teams meeting!",
       final: false,
       slack:
-        "Not everyone is a guy, 'guys' is a gendered pronoun. We recommend an alternative like 'crew'. We appreciate your help in building an inclusive environment at Dev!",
+        "Not everyone is a 'Guy'.. 'Guys' is a gendered pronoun. We recommend an alternative like 'crew'. We appreciate your help in building an inclusive environment at Dev!",
     },
     {
       id: 2,
       level_id: 3,
-      text: "Me again, Just checking in that you, still in the building? I left my swipe card in the basement if you're still around maybe you could grab for me? Sorry should've mentioned before you turned all the lights off. Don't mind the javascript carnival gang, they get a little restless down there. But anyways let me know!",
+      text: "Me again, Just checking in that you're still in the building? I left my swipe card in the basement if you're still around maybe you could grab for me? Sorry should've mentioned before you turned all the lights off. Don't mind the javascript carnival gang, they get a little restless down there. But anyways let me know!",
       final: false,
       slack:
         'You asked me to remind you about this message from @facilitator-piwakawaka-23',
@@ -34,8 +34,7 @@ exports.seed = async function (knex) {
       level_id: 5,
       text: "Oh, you made it out of the building? Well, thats great then! uhh... i suppose we'll see you tomorrow then.",
       final: false,
-      slack:
-        '👍 I will remind you of #teams-meeting at 10:30AM tomorrow morning.',
+      slack: ' 👍 I will remind #teams-meeting at 10:30AM tomorrow morning.',
     },
     {
       id: 5,
